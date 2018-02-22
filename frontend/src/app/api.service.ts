b574247ba77d5d48666f94fd68bbad59e885235d
Luis Espinoza <luis.espinoza@buildbinder.com>
2018-02-22 13:23:46 -0700

    improve TS syntax and commente api service

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -3,14 +3,39 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ApiService {
-API_URL : string = "http://localhost:3000/";
+  API_URL = 'http://localhost:3000/';
 
   constructor(public http: HttpClient) { }
-
+  /**
+   * @param path HTTP GET path 'todos'
+   * @return get api data
+   * */
   public get(path) {
+      return this.http.get(this.API_URL + path);
+  }
+  /**
+   * @param  path HTTP POST 'todos'
+   * @param  body data to post 'any object'
+   * @returns post data to api
+   */
+  public post(path: string, body: any) {
+    return this.http.post(this.API_URL + path, body);
 
-      var endpoint = this.API_URL + path;
-      return this.http.get(endpoint);
+  }
+  /**
+   * @param  path HTTP DELTE 'todos/1'
+   * @returns delete data to api
+   */
+  public delete(path: string) {
+    return this.http.delete(this.API_URL + path);
 
+  }
+  /**
+   * @param  path HTTP UPDATE 'todos'
+   * @param  body update to post 'any object'
+   * @returns update data to api
+   */
+  public update(path: string, body: any) {
+    return this.http.put(this.API_URL + path, body);
   }
 }
--- frontend/src/app/todo-list/todo-list.component.ts
+++ frontend/src/app/todo-list/todo-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
-import { Todo } from '../todo';
+import { Todo } from '../models/todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -10,12 +10,12 @@ import { Todo } from '../todo';
 })
 export class TodoListComponent implements OnInit {
 
-public columns = ['id','name'];
-public rows : Array<Todo>; 
-  constructor(public apiService: ApiService , public router : Router) { }
+public columns = ['id', 'name'];
+public rows: Array<Todo>;
+  constructor(public apiService: ApiService , public router: Router) { }
 
   ngOnInit() {
-  	this.apiService.get("todos").subscribe((data : Todo[])=>{
+    this.apiService.get('todos').subscribe((data: Todo[]) => {
     console.log(data);
     this.rows = data;
     });
