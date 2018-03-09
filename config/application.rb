948e2ea8de6dd7da375ea589a6f7fbf9c2fc72f0
MacBook Pro Developer <4freelancerdevelopers@gmail.com>
2018-03-08 23:44:57 -0700

    rails-nameerror-uninitialized-constant-class-solution

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module RubyApiAngularApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     # load lib folder scripts
-    config.autoload_paths << Rails.root.join('lib')
+    #config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
