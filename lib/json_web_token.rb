7ab865217029dacb940e0ab5b2568bd76eaac4b0
MacBook Pro Developer <4freelancerdevelopers@gmail.com>
2018-03-08 23:07:43 -0700

    comment require

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,11 +3,11 @@ class JsonWebToken
 		# takes three parameters -- the user id, the expiration time (1 day), and the unique base key of your Rails application -- to create a unique token
 		# we must set all important values that we want to return in jwt
 		def encode(payload, exp = 24.year.from_now) 
-			payload[:iss] = 'masterapi.facware.com'
-			payload[:sub] = 'api rails base'
+			# payload[:iss] = 'masterapi.facware.com'
+			# payload[:sub] = 'api rails base'
 			payload[:exp] = exp.to_i 
-			payload[:aud] = 'All' #to identify the audience, e.g. myapp.com domain
-			payload[:iat] = Time.zone.now.to_i #The "iat" (issued at) claim identifies the time at which the JWT was issued.
+			# payload[:aud] = 'All' #to identify the audience, e.g. myapp.com domain
+			# payload[:iat] = Time.zone.now.to_i #The "iat" (issued at) claim identifies the time at which the JWT was issued.
 			
 			JWT.encode(payload, Rails.application.secrets.secret_key_base) 
 		end 
